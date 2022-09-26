c06370faf457f82b8d38f348f2be0dd8a3126198
Felipe <machadofelipe2016@outlook.com>
2022-09-26 15:24:49 -0300

    c4-adding another thign

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,16 @@
+require("dotenv").config()
 
-const express = require("express.js")
+const express = require("express")
 const app = express()
+
+const port = process.env.PORT ;
 
 const path = require("path")
 const cors = require("cors")
 
+
 // set up to define receive all dates to back for frontend
-app.use(express.join())
+app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
 // now i have together it work with cors
@@ -14,3 +18,12 @@ app.use(cors({credentials:true, origin:"http://localhost:3000"}))
 
 //upload directory
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
+
+
+// router here
+const route = require("./Routes/route.js")
+app.use(route)
+
+app.listen(port, () => {
+    console.log(`Port its working here ${port}`)
+})
