104b20a54fddc57196bc33b41e64a07ad49879b8
Felipe <machadofelipe2016@outlook.com>
2022-09-28 13:13:26 -0300

    c14-finished up all product model

---
+++ backend/models/Product.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose")
+const {Schema} = mongoose;
+
+const AddProduct = new Schema({
+   name: String,
+   describe: String,
+   price: Number,
+   inventory: Number,
+   category: String,
+   brand:String
+},{
+    timestamps: true
+})
+
+
+const Product = mongoose.model("Produto", AddProduct)
+module.exports = Product;
