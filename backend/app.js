5c3a9b79927171df76608104b408da08f711ad4a
Felipe <machadofelipe2016@outlook.com>
2022-09-28 16:06:42 -0300

    c21-rearrange code and adding new thigns inside app

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,10 @@ require("dotenv").config()
 const express = require("express")
 const app = express()
 
-const port = process.env.PORT ;
-
 const path = require("path")
 const cors = require("cors")
 
+const port = process.env.PORT;
 
 // set up to define receive all dates to back for frontend
 app.use(express.json())
@@ -19,9 +18,12 @@ app.use(cors({credentials:true, origin:"http://localhost:3000"}))
 //upload directory
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
 
+// connection with mongodb
+require("./settings/db.js")
 
 // router here
 const route = require("./Routes/route.js")
+const { dirname } = require("path")
 app.use(route)
 
 app.listen(port, () => {
