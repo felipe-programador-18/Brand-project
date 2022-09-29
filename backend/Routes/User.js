92b6b190ada3d16e6d6834446a61b42cb9e7aca2
Felipe <machadofelipe2016@outlook.com>
2022-09-29 14:37:18 -0300

    c29-adding a new middleware valitor to care of my register and login too

--- backend/Routes/User.js
+++ backend/Routes/User.js
@@ -2,19 +2,23 @@ const express = require("express")
 const router = express.Router()
 
 // controllers 
-const {register} = require("../controllers/user")
+const {register, loginControlled} = require("../controllers/user")
 
 //middleware
-const {userValUser } = require("../middware/user.validation")
+const validator = require("../middware/handlevalidator")
+const {userValUser, login } = require("../middware/user.validation")
 
 
-router.post("/register", userValUser() ,register)
-router.post("/login")
+router.post("/register", userValUser(), validator ,register)
+router.post("/login",login(),validator, loginControlled)
 
 
 
 
 router.use("/api/users/register",register)
+router.use("api/users/login", loginControlled)
+
+
 //router.use("/api/users/categoryProduto")
 
 module.exports = router;
