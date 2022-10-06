02ecfd6df51c62d981057493fbdcf8271ee83ba1
Felipe <machadofelipe2016@outlook.com>
2022-10-06 15:13:19 -0300

    c31-create a structure to add picture user

--- backend/middware/imageUpload.js
+++ backend/middware/imageUpload.js
@@ -2,9 +2,42 @@ const multer = require("multer")
 const path = require("path")
 
 
-const imageStory = ()=> {
+const imageStory = multer.diskStorage({
+    destination: (req, file, cb) => {   
+     let folder=""
+        
+     if(req.baseUrl.includes("users")){
+       folder ="users"
+     }else if(req.baseUrl.includes("produtos")){
+      folder ="produtos" 
+     }
+     
+     // this structure its works for save picture
+     cb(null, `uploads/${folder}/`)
+      
+    },
+// this structure generate date and generate new name of file !!
+    filename: (req,file, cb) =>{
+     cb(null, Date.now() + path.extname(file.originalname))
+    }
+})
 
+const updateImage = multer({
+    storage: imageStory,
+    fileFilter: (req, file, cb) => {
+     if(!file.originalname.match(/\.(png|jpg)$/)){
+      
+        //upload of jpg and png file
+            
+        return cb( new Error("Por favor envie apenas fotos, jpg ou png!") )
+        }
+     cb(undefined, true)
+
+    }
+})
+
+
+
+module.exports ={
+ updateImage
 }
-
-
-const updateImage = ""
