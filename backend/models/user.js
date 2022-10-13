a9ac0780fd03199b964fd6795353f4cffc9b4881
Felipe <machadofelipe2016@outlook.com>
2022-10-13 14:47:57 -0300

    c41-I been forgot adding bio into my user

--- backend/models/user.js
+++ backend/models/user.js
@@ -5,7 +5,8 @@ const userSchema = new Schema({
   name: String,
   email: String,
   password:String,
-  profileImage:String,  
+  profileImage:String,
+  bio: String  
 }, {
     timestamps:true
 })
