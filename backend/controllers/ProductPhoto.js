ad7482f9155c7242e8cf8d8a62b3d8d3768c194b
Felipe <machadofelipe2016@outlook.com>
2022-10-14 16:22:41 -0300

    c48-add more date into my product controllers

---
+++ backend/controllers/ProductPhoto.js
@@ -0,0 +1,33 @@
+const ProductPhoto = require("../models/Product")
+const User = require("../models/user")
+const mongoose = require("mongoose")
+
+//insertUser Picture
+const InsertPhotoProduct = async(req,res) =>{
+   const{ name } = req.body
+   const{ image } = req.file.filename
+
+    const reqUser =req.user
+    const user = await User.findById(reqUser._id)
+    console.log("Hi my user here!!!", user)
+
+    const newProduct = await ProductPhoto.create({
+        name,
+        image,
+        userId:user._id,
+        userName: user.name 
+    })
+   
+    if(!newProduct){
+        res.status(422).json({errors:["not is possible adding photo, please try more later!"]})
+        return
+    }
+
+    res.status(201).json(newProduct)
+    
+}
+
+
+module.exports = {
+    InsertPhotoProduct
+}
