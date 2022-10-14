02d8ec5c4d91c222a94255ea3f72e29effbb4d11
Felipe <machadofelipe2016@outlook.com>
2022-10-14 16:23:45 -0300

    c49-adding more router and get prepare buildind all strucuture for another router

--- backend/Routes/route.js
+++ backend/Routes/route.js
@@ -4,7 +4,9 @@ const route = express.Router()
 
 // dont forget here i can add two router brand and user!!
 route.use("/api/users", require("./User") )
+route.use("/api/product", require("./Productroute"))
 route.use("/api/brand", require("./brand"))
+route.use("/api/category", require("./Categoryroute"))
 
 
 route.get("/",(req,res) => {
