9acd3fcadac02eda50226cc03c63fc0897b165f4
Felipe <machadofelipe2016@outlook.com>
2022-10-14 16:18:40 -0300

    c45-starting create a new product controllers

--- backend/Routes/Productroute.js
+++ backend/Routes/Productroute.js
@@ -2,6 +2,23 @@ const express = require("express")
 const  router = express.Router()
 
 
+//controllers
+const {InsertPhotoProduct} = require("../controllers/ProductPhoto")
+
+
+//middleware
+const {photoInsertValidation} = require("../middware/photoValidation")
+const AuthGuard = require("../middware/authGuard")
+const validator = require("../middware/handlevalidator")
+
+const {updateImage} = require("../middware/imageUpload")
+
+router.post("/" ,AuthGuard, 
+updateImage.single("image"),
+photoInsertValidation(),
+validator,
+InsertPhotoProduct)
+
 
 
 module.exports = router
