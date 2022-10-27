7f24bb83f1ec3e0f41e0ab93d590c7a249d7e046
Felipe <machadofelipe2016@outlook.com>
2022-10-27 14:08:36 -0300

    c65-get start the add new functionality and also caught modals user and brand to getting adding brands

---
+++ backend/controllers/Brand.js
@@ -0,0 +1,14 @@
+const tradeBrands = require("../models/Brands")
+const User = require("../models/user")
+
+const mongoose = require("mongoose")
+
+const PhotoBrand = async(req, res) => {
+
+}
+
+
+
+module.exports ={
+    PhotoBrand
+}
