60fce4ec2cc7984c3b52e0d5c7d4d96588ef9843
Felipe <machadofelipe2016@outlook.com>
2022-11-16 10:38:36 -0300

    c103-start create structure

---
+++ backend/controllers/Category.js
@@ -0,0 +1,14 @@
+const caProduct = require("../models/CategoriesProduct")
+const User = require("../models/user")
+
+const mongoose = require("mongoose")
+
+const testingCategory = async(req,res) =>{
+
+}
+
+
+
+module.exports ={
+  testingCategory
+}
