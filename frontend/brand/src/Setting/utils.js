6071b652c406def16ed99912706974cbb849dabc
Felipe <machadofelipe2016@outlook.com>
2022-11-17 13:39:25 -0300

    c121-finally of creating funcion

--- frontend/brand/src/Setting/utils.js
+++ frontend/brand/src/Setting/utils.js
@@ -15,10 +15,22 @@ export const reqConfig = (method, data, token=null, image=null) => {
       }
 
     }else if(method === 'DELETE' || data === null){
-      
+      config={
+       method: method,
+       headers:{}
+      }
     }else{
-
+      config ={
+        method,
+        body: JSON.stringify(data),
+        headers:{
+            "Content-Type":"application/json"
+        }
+      }
     }
-
-
+    
+    if(token){
+        
+    }
+    
 }
