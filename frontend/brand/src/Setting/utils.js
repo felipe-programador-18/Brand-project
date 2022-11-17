5f44f1b761d08b45edb113f657896cc0ca498ca9
Felipe <machadofelipe2016@outlook.com>
2022-11-17 13:27:15 -0300

    c120-creating and add a new to function

--- frontend/brand/src/Setting/utils.js
+++ frontend/brand/src/Setting/utils.js
@@ -2,3 +2,23 @@ export const api = "http://localhost:5000/api"
 
 // Reminder way to save uploads of backend!
 export const uploads = "http://localhost:5000/uploads"
+
+
+export const reqConfig = (method, data, token=null, image=null) => {
+
+    let config
+    if(image){
+      config ={
+       method:method,
+       body: data ,
+       headers:{} 
+      }
+
+    }else if(method === 'DELETE' || data === null){
+      
+    }else{
+
+    }
+
+
+}
