3fe943c0dafb380b590e48181baed915c6a34518
Felipe <machadofelipe2016@outlook.com>
2023-02-18 20:02:25 -0300

    c137-to finish my setting up

---
+++ frontend/brand/src/settings/utils.js
@@ -0,0 +1,35 @@
+export const Api = "http://localhost:5000/api"
+
+export const uploadsProducts = "http://localhost:5000/api"
+
+export const CreateAllRequest = (method, data, token=null , image=null ) => {
+    let config;
+    if(image) {
+     config= {
+      method:method,
+      body:data,
+      headers:{}
+     }  
+    }else if (method === 'DELETE' || data === null ) {
+     config ={
+        method:method,
+        headers:{}
+     }
+    }else {
+     // this case with me receive dates!
+     config ={
+       method,
+       body: JSON.stringify(data),
+       headers:{
+        "Content-Type":"application/json"
+       }
+     }
+    }
+
+    
+    if(token){
+     config.headers.Authorization = `Bearer ${token}`
+    }
+   
+   return config
+}
