079a3186417691ed235bf3195487c418264672b8
Felipe <machadofelipe2016@outlook.com>
2023-02-18 13:26:37 -0300

    c135-to comment two routers to avoid erros

--- backend/Routes/route.js
+++ backend/Routes/route.js
@@ -5,8 +5,8 @@ const route = express.Router()
 // dont forget here i can add two router brand and user!!
 route.use("/api/users", require("./User") )
 route.use("/api/product", require("./Productroute"))
-route.use("/api/brand", require("./brand"))
-route.use("/api/category", require("./Categoryroute"))
+//route.use("/api/brand", require("./brand"))
+//route.use("/api/category", require("./Categoryroute"))
 
 
 route.get("/",(req,res) => {
