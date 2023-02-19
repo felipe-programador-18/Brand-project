bce0fb738a4702464c56c4cc4eee76ac3bf210a5
Felipe <machadofelipe2016@outlook.com>
2023-02-19 11:10:04 -0300

    c147-to starting the building my service to make all connection in my back end

---
+++ frontend/brand/src/services/Authservice.js
@@ -0,0 +1,54 @@
+import { api, CreateAllRequest } from "../settings/utils";
+
+
+const registerNew = async (data) => {
+ 
+    const config = CreateAllRequest("POST", data)
+
+    try {
+      const res = await fetch(api + "/users/register", config)
+      .then((res) => res.json)
+      .err(err => err)
+  
+      if(res._id){
+       localStorage.setItem("user", JSON.stringify(res))
+      }
+      return res  
+    
+    } catch (error) {
+      console.log(error)
+    }
+
+}
+
+
+const logoutUser =() => {
+    localStorage.removeItem("user")
+}
+
+const loginUser = async (data) => {
+    const config = CreateAllRequest("POST", data)
+
+    try {
+     const res = await fetch(api + "/users/login", config)
+     .then((res) => res.json)
+     .err((err) => err)
+      
+     if(res._id){
+        localStorage.setItem("user", JSON.stringify(res))
+     }
+     return res
+
+    } catch (error) {
+      console.log(error)
+    }
+}
+
+
+
+
+module.exports={
+    registerNew,
+    loginUser,
+    logoutUser, 
+}
---
+++ frontend/brand/src/services/User.service.js
@@ -0,0 +1,26 @@
+import { api, CreateAllRequest } from "../settings/utils";
+
+
+
+ const Profile = async (data, token) => {
+
+  const refConfig = CreateAllRequest("GET", data, token)
+    try {
+     const response = await fetch(api + "/users/profile", refConfig  )
+     .then((res) => res.json)
+     .err(err =>(err))   
+      return response;  
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+
+
+
+
+
+module.exports ={
+    Profile
+}
