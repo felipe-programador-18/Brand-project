e1e66a2fe5909fe3931922ccfb3ad37074ee004e
Felipe <machadofelipe2016@outlook.com>
2023-02-21 12:38:54 -0300

    c167-to get start to prepared my home

---
+++ frontend/brand/src/pages/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Container from 'react-bootstrap/esm/Container'
+
+
+
+const Home = () => {
+
+
+
+    return( <Container>
+    <h1>My home here..</h1>
+
+    </Container> )
+
+}
+
+export default Home
