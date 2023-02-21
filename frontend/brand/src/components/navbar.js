9e5e97a79d19e10b69a4b1281d50d27fcb3708dd
Felipe <machadofelipe2016@outlook.com>
2023-02-21 13:22:47 -0300

    c169-to fixed and to try something new idea

--- frontend/brand/src/components/navbar.js
+++ frontend/brand/src/components/navbar.js
@@ -1,32 +1,66 @@
-import React from 'react' 
+import {useState, useEffect} from 'react' 
+import { useSelector, useDispatch } from 'react-redux';
+import  {useNavigate} from 'react-router-dom'
+
+import {UseAuth} from '../hooks/Auth'
 
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
-import {NavLink} from 'react-router-dom'
+import  {BsSearch} from 'react-icons/bs'
 
+import {NavLink,Link} from 'react-router-dom'
+import {logout} from '../slices/Authslice'
 
 const NavOtherBar = () => {
+  
+  const {auth} = UseAuth()
+ // const {user} = useSelector((state) => state.auth)
+  
+  const [query, setQuery] = useState("")
+
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  
+  const HandleLogout = () => {
+    dispatch(logout())
+    dispatch()
+    navigate("/login")
+  }
+
+
+  const HandleSearch = (e) => {
+    e.preventDefault()
+    
+    if(query){
+      return navigate(`/search?=q${query}`)
+    }
+    
+    setQuery("")
+  }
+
+
 
     return (
-  <Navbar bg="light mx-6 "  expand="lg">
-   
-      <Navbar.Brand href="#home">Brand-Project</Navbar.Brand>
+  
+  <Navbar bg="light mx-6 "  expand="lg"> 
+      <Navbar.Brand href="home">Brand-Project</Navbar.Brand>
+      
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
           
-          <Nav.Link href='#home' >Home</Nav.Link>
+          <Nav.Link href='/home' >Home</Nav.Link>
           <Nav.Link href="/login" >Sing-in</Nav.Link>
          
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="/about" >About</NavDropdown.Item>
+          <NavDropdown title="Information" id="basic-nav-dropdown">
+            <NavDropdown.Item href="/about" >Profile</NavDropdown.Item>
             <NavDropdown.Item href="#action/3.2">
-              Github
+              Product
             </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Linkedin</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Users</NavDropdown.Item>
             <NavDropdown.Divider />
             <NavDropdown.Item href="#action/3.4">
               Separated link
@@ -35,7 +69,10 @@ const NavOtherBar = () => {
         </Nav>
       </Navbar.Collapse>
    
-  </Navbar> )
+  </Navbar> 
+  
+ 
+  )
 
 
 
