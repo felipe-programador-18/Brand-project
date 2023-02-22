452153d73ba7cbcb34afb4d0bbc544250fa9f6f8
Felipe <machadofelipe2016@outlook.com>
2023-02-22 11:36:13 -0300

    c176-to finish up my userslice

--- frontend/brand/src/slices/Userslice.js
+++ frontend/brand/src/slices/Userslice.js
@@ -31,3 +31,57 @@ export const GetUserBy = createAsyncThunk("user/get", async (id,thunkAPI) => {
      
     return data;
 })
+
+
+
+export const userSlice = createSlice({
+    name:"user",
+    initialState,
+    reducers:{
+    resetMessage : (state) => {
+        state.message = null
+      }
+    },
+
+    extraReducers: (builder) => {
+        builder
+        .addCase(Profile.pending, (state) => {
+            state.loading = true;
+            state.error = null;
+        }).addCase(Profile.fulfilled, (state,action) => {
+            state.loading = false;
+            state.success = true;
+            state.error = null;
+            state.user = action.payload;
+        }).addCase(UpdateProfile.pending, (state) => {
+            state.loading = true;
+            state.error = null;
+        }).addCase(UpdateProfile.fulfilled, (state,action) => {
+            state.loading = false;
+            state.success = true;
+            state.error = null;
+            state.user = action.payload;
+            state.message = "Usuário atualizado com sucesso!"
+        }).addCase(UpdateProfile.rejected, (state,action) => {
+            state.loading = false;
+            state.error = action.payload;
+            state.user ={};
+        }).addCase(GetUserBy.pending, (state) => {
+            state.loading = true;
+            state.error = null;
+        }).addCase(GetUserBy.fulfilled, (state,action) => {
+            state.loading = false;
+            state.success = true;
+            state.error = null;
+            state.user = action.payload;
+        })
+    }
+})
+
+
+//reminder reducer always have state and action!
+
+export const {resetMessage} = userSlice.actions;
+export default userSlice.reducer;
+
+
