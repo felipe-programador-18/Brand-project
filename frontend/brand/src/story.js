c20d5b0d5c985efdd2a581106e4e69add281a280
Felipe <machadofelipe2016@outlook.com>
2023-02-22 11:35:15 -0300

    c175-to add my one reducer to my store

--- frontend/brand/src/story.js
+++ frontend/brand/src/story.js
@@ -2,10 +2,11 @@ import { configureStore } from '@reduxjs/toolkit'
 
 
 import authReducer from './slices/Authslice'
-
+import userReducer from './slices/Authslice'
 
 export const store = configureStore({
     reducer:{
-      auth: authReducer  
+      auth: authReducer,
+      user: userReducer  
     }
 })
