9b4bf6048004fad9cca9156e29f00662e3afa857
Felipe <machadofelipe2016@outlook.com>
2023-02-23 09:18:01 -0300

    c183-to finish up my message component

---
+++ frontend/brand/src/components/Message.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+
+const MessageError = ({msg, type}) => {
+    return (<div className={`message ${type}`} >
+     <p>{msg}</p>
+    </div>)
+}
+
+export default MessageError
