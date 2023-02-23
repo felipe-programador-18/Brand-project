89fbd3a7424fc4917a7b370f20d2422bb13c636a
Felipe <machadofelipe2016@outlook.com>
2023-02-23 11:28:00 -0300

    c199-to repairing the issues on my code

--- frontend/brand/src/services/Authservice.js
+++ frontend/brand/src/services/Authservice.js
@@ -4,11 +4,10 @@ import { api, CreateAllRequest } from "../settings/utils";
 const registerNew = async (data) => {
  
     const config = CreateAllRequest("POST", data)
-
     try {
       const res = await fetch(api + "/users/register", config)
-      .then((res) => res.json)
-      .err(err => err)
+      .then((res) => res.json())
+      .catch((err) => err)
   
       if(res._id){
        localStorage.setItem("user", JSON.stringify(res))
@@ -31,7 +30,7 @@ const loginUser = async (data) => {
     try {
      const res = await fetch(api + "/users/login", config)
      .then((res) => res.json())
-     .err((err) => err)
+     .catch((err) => err)
       
      if(res._id){
         localStorage.setItem("user", JSON.stringify(res))
