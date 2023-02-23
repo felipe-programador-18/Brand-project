ac439ad8e4e1d684430f8a0b96e1dd5b8e84667c
Felipe <machadofelipe2016@outlook.com>
2023-02-23 11:25:51 -0300

    c197-to create my profile folder

---
+++ frontend/brand/src/pages/Profile/ProfileUser.js
@@ -0,0 +1,37 @@
+import React, {useState, useEffect} from 'react'
+
+import {useDispatch, useSelector} from 'react-redux'
+
+import Container from 'react-bootstrap'
+
+
+const ProfileUser = () => {
+
+
+const HandleSubmit = (e) => {
+  e.preventDefault()
+}
+
+
+const HandleFile = () => {
+
+}
+
+const HandleDeleted = () => {
+
+}
+
+return ( <Container>
+
+
+<form>
+
+
+</form>
+
+</Container> )
+
+
+
+
+}
