50332f7cf12eaf9782903c3d53086fc2f5020881
Felipe <machadofelipe2016@outlook.com>
2023-02-24 10:55:26 -0300

    c209-to get start to building my profile structure

--- frontend/brand/src/pages/Profile/ProfileUser.js
+++ frontend/brand/src/pages/Profile/ProfileUser.js
@@ -2,11 +2,14 @@ import React, {useState, useEffect} from 'react'
 
 import {useDispatch, useSelector} from 'react-redux'
 
+import { GetUserBy } from '../../slices/Userslice'
 import Container from 'react-bootstrap'
 
 
 const ProfileUser = () => {
 
+ const [title, setTitle] = useState("")
+ const [image, setImage] = useState("")
 
 const HandleSubmit = (e) => {
   e.preventDefault()
