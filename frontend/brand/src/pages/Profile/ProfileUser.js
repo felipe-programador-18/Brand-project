425058ddd6ef2e6e265256fb00e7cfd25c113419
Felipe <machadofelipe2016@outlook.com>
2023-02-27 10:24:49 -0300

    c222-to come to put on the hands to finish my profile user up

--- frontend/brand/src/pages/Profile/ProfileUser.js
+++ frontend/brand/src/pages/Profile/ProfileUser.js
@@ -1,9 +1,20 @@
-import React, {useState, useEffect} from 'react'
+import {useState, useEffect} from 'react'
+import {Link, useParams} from  'react-router-dom'
+
+import { uploadsProducts } from '../../settings/utils'
+
+import {resetMessage, 
+  PublicProduct,
+  EditProductBrand,
+  getProductUser,
+  DeletedProduct}  from "../../slices/Productslice"
 
 import {useDispatch, useSelector} from 'react-redux'
 
+
+
 import { GetUserBy } from '../../slices/Userslice'
-import Container from 'react-bootstrap'
+import Container from 'react-bootstrap/Container'
 
 
 const ProfileUser = () => {
@@ -38,3 +49,5 @@ return ( <Container>
 
 
 }
+
+export default ProfileUser
